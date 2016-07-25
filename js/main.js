7e0a387c134d63b0069065e2924254caf8fc02e5
Leo Vidal <leo@leovidal.es>
2016-07-25 14:21:15 +0200

    Commit start complemented

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 // main JavaScript
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+    	x.innerHTML = 'works';
+        // navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+# SCOUT IN project
+
+
+## Description
+
+This app brings users automatic geolocation in order to get interesting nearby places whitin a radio of 5 miles.
+
+### Team members
+
+
+Ana Gamito
+
+
+Ángel San Nicolás
+
+
+Leonardo Rdgz. Vidal
+email: leo@leovidal.es
